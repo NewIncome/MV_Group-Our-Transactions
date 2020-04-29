d37f034a2401135fd19214442c38e9ab8c7aa903
NewIncome <alfredo_cardenas@live.com>
2020-04-29 15:11:05 -0500

    Add the rails routes to the routes.rb file for ease of use.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#login'
+  resources :users, only: [:new, :create, :show]
+  resources :groups, only: [:index, :show, :new, :create]
+  resources :transactions, only: [:index, :show, :new, :create], as: 'trns'
 end
+
+# ROUTES:
+#      root GET     /                               users#login
+#     users POST    /users(.:format)                users#create
+#  new_user GET     /users/new(.:format)            users#new
+#      user GET     /users/:id(.:format)            users#show
+#    groups GET     /groups(.:format)               groups#index
+#           POST    /groups(.:format)               groups#create
+# new_group GET     /groups/new(.:format)           groups#new
+#     group GET     /groups/:id(.:format)           groups#show
+#      trns GET     /transactions(.:format)         transactions#index
+#           POST    /transactions(.:format)         transactions#create
+#   new_trn GET     /transactions/new(.:format)     transactions#new
+#       trn GET     /transactions/:id(.:format)     transactions#show
