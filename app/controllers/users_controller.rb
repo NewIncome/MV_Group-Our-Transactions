d7a1b358837bbcb5966ba6f212062e9e4a3cc8da
NewIncome <alfredo_cardenas@live.com>
2020-05-03 19:09:00 -0500

    Make better the Navbar; Add code for the user login methods; Fix the routes file.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,31 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
+  end
+
+  def create
   end
 
   def show
   end
+  
+  # basic User Session methods
+  def login; end
 
-  def login
+  def logincreate
     @user = User.find_by(name: params[:name])
+    if @user
+      session[:user_id] = @user.id
+      flash[:success] = "#{@user.name} you are logged in!"
+      redirect_to html: 'Logged in' # @user
+    else
+      flash.now[:danger] = "Please sign up first"
+      render html: "User doesn't exist"
+    end
   end
 
+  def logout
+    session.delete(:user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'users#login'
-  get '/signup', to: 'users#new'
-  resources :users, only: [:create, :show]
+  get '/login', to: 'users#login'
+  post '/login', to: 'users#logincreate'
+  delete '/logout', to: 'users#destroy'
+  resources :users, only: [:new, :create, :show]
   resources :groups, only: [:index, :show, :new, :create]
   resources :transactions, only: [:index, :show, :new, :create], as: 'trns'
+  root 'users#login'
 end
 
 # ROUTES:
