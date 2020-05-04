593747eb7d7f66c22b09402a65994615e42476ab
NewIncome <alfredo_cardenas@live.com>
2020-05-03 19:17:03 -0500

    Add the signup route; Add the correct temp root path link in _header; Add a link to signup in login.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'users#new'
   get '/login', to: 'users#login'
   post '/login', to: 'users#logincreate'
   delete '/logout', to: 'users#destroy'
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:create, :show]
   resources :groups, only: [:index, :show, :new, :create]
   resources :transactions, only: [:index, :show, :new, :create], as: 'trns'
   root 'users#login'
