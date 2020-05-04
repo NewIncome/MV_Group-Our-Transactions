fd15341315242b21ef08306b83545b92fec2e361
NewIncome <alfredo_cardenas@live.com>
2020-05-04 12:05:00 -0500

    Add code for the creation of a new group.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,28 @@
 class GroupsController < ApplicationController
+  
   def new
     @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      flash[:success] = "'#{@group.name}' group created successfully!"
+      redirect_to @group
+    else
+      render 'new'
+    end
   end
 
   def show
   end
 
   def index
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
