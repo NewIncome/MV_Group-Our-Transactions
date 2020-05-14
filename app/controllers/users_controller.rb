0f1b17774e19ead822c761fb790d13770da0d0d6
NewIncome <alfredo_cardenas@live.com>
2020-05-13 23:01:20 -0500

    Add the titles and remove the links in the header. and last minute quick fixes: - quickFix for flash messages showing below button for new; - quickFix for session of a new user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = 'Account created successfully!'
       redirect_to @user
     else
