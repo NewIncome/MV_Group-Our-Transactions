5b01a823ca8dfc4335a8e215a6c70ff08332ea62
NewIncome <alfredo_cardenas@live.com>
2020-05-21 20:42:56 -0500

    - Add some coments to all model tests; - Clear of created classes in the 3 unit tests; - Add the last unit tests for the Transaction model.

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -10,16 +10,19 @@ RSpec.describe Group, type: :model do
       @group1 = Group.new(name: 'Group01', user_id: @user.id)
     end
 
+    # 1st test
     it 'creates a new group' do    
       @group1.save
       expect(@group1.valid?).to eq(true)
     end
 
+    # 2nd test
     it "doesn't create a group with an empty name" do
       @group2 = Group.create(name: '')
       expect(@group2.valid?).to eq false
     end
 
+    # 3rd test
     it 'creates a group with only a length of min:4 and max:30' do
       @group2 = Group.create(name: '')
       expect(@group2.errors.full_messages[1]).to include 'short'
@@ -27,17 +30,27 @@ RSpec.describe Group, type: :model do
       expect(@group2.errors.full_messages[0]).to include 'long'
     end
 
+    # 4th test
     it 'creates a group only if the name is unique' do
       @group1.save
       @group2 = Group.create(name: "#{@group1.name}")
       expect(@group2.valid?).to eq false
     end
 
+    # 5th test
     it 'creates a group not needing an icon' do
       @group1.save
       expect(@group1.icon.nil? & @group1.valid?).to eq true
     end
+
+    # 6th test
+    it "has to belong to a user" do
+      @group1.user_id = nil
+      @group1.save
+      expect(@group1.valid?).to eq false
+    end
   end
 
-  after(:all) { Group.destroy_all }
+  # Clear created User(s) and Group(s)
+  after(:all) { User.destroy_all & Group.destroy_all }
 end
